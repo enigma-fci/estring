bdc6eb5fd6feb10beddcd1ed4218eef1cdfc228d
Mahmoud Ramadan <47820058+Mahmoud-Hashad@users.noreply.github.com>
2019-08-16 14:45:42 +0200

    Update estring.h

--- estring.h
+++ estring.h
@@ -26,15 +26,34 @@ class estring
     bool operator == (const estring& str);
     friend bool operator == (const char str[],const estring& ob);
     
+    // plus equal operator overloading
+    void operator += (const char str[]);
+    void operator += (const estring& str);
+
+    // e_includes method
+    bool e_includes(const char str[]);
+    bool e_includes(const estring& str);
+
+    // endsWith method
+    bool endsWith(const char str[], int index = 0);
+    bool endsWith(const estring& str, int index = 0);
+
+    // pad end method
+    estring padEnd(int len, const char str[] = " ");
+    estring padEnd(int len, const estring& str);
+
+    estring trimRight(void);   // trim Right method
+    estring e_reverse(void);   // e_reverse method
+    estring toLoweCase(void);  // to lower case method
     estring e_erase(int start_i, int end_i); // erase method
     
     // cin >> & cout << overloading
     friend ostream & operator << (ostream &out, const estring &str);
     friend istream & operator >> (istream &in,  estring &str);
   
-    	int length();
-  	char charAt(int x);
-	int charCodeAt(int x);
+    int length();
+		char charAt(int x);
+		int charCodeAt(int x);
   	int indexOf(estring e, int x = 0);
   	int lastIndexOf(estring e, int x = 0);
   	estring trim();
