e9cd942a7aca47de766cdc9fca09112c7925c5e8
abdomosa1999 <37039899+abdomosa1999@users.noreply.github.com>
2019-08-16 14:15:00 +0200

    Update estring.cpp

--- estring.cpp
+++ estring.cpp
@@ -194,6 +194,148 @@ istream & operator >> (istream &in, estring &str) {
   return in;
 }
 
+int estring::length()
+{
+	int length;
+	for (length = 0; str[length] != '\0'; length++);
+	return length;
+}
+
+char estring::charAt(int x)
+{
+
+	return str[x];
+}
+
+int estring::charCodeAt(int x) {
+	return str[x];
+}
+
+int estring::indexOf(estring e, int x)
+{
+	// get length of the new string
+	int le, lo = this->length();
+	bool test;
+	for (le = 0; e.str[le] != '\0'; le++);
+	for (int i = x; i < lo; i++) {
+		test = true;
+		for (int j = 0; j < le; j++) {
+			if (this->str[i + j] != e.str[j]) {
+				test = false;
+				break;
+			}
+		}
+		if (test) return i;
+	}
+	return -1;
+}
+
+int estring::lastIndexOf(estring e, int x)
+{
+	int le, lo = this->length();
+	bool test;
+	for (le = 0; e.str[le] != '\0'; le++);
+	for (int i = lo - 1; i >= x; i--) {
+		test = true;
+		for (int j = le - 1; j >= 0; j--) {
+			if (this->str[i - j] != e.str[j]) {
+				test = false;
+				break;
+			}
+		}
+		if (test) return i;
+	}
+	return -1;
+}
+
+estring estring::trim()
+{
+	int l = this->length(), lss = 0, lse = 0;
+	char* temp = this->str;
+	for (int i = 0; i < l; i++)
+		if (str[i] == ' ')
+			lss++;
+		else
+			break;
+	for (int i = l - 1; i >= 0; i--)
+		if (str[i] == ' ')
+			lse++;
+		else
+			break;
+	this->str = new char[l - (lss + lse)];
+	for (int i = lss, j = 0; i < l- lse; i++, j++)
+		this->str[j] = temp[i];
+	this->str[l - (lss + lse)] = '\0';
+	delete[] temp;
+	return this->str;
+}
+
+estring estring::padStart(int x, estring e)
+{
+	for (int i = 0; i < x; i++) {
+		str--;
+		str[0] = e.str[0];
+	}
+	return this->str;
+}
+
+estring estring::toUpperCase()
+{
+	int l = length();
+	for (int i = 0; i < l; i++)
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+	return str;
+}
+
+int estring::count(estring e)
+{
+	int count = 0, le = e.length(), lo = this->length();
+	bool test;
+	for (int i = 0; i < lo; i++) {
+		test = true;
+		for (int j = 0; j < le; j++) {
+			if (this->str[i + j] != e.str[j]) {
+				test = false;
+				break;
+			}
+		}
+		if (test) count++;
+	}
+	return count;
+
+}
+
+bool estring::operator!=(estring e)
+{
+	int lo = this->length(), le = e.length();
+
+	if (lo != le) return true;
+	// compare each char with it corresponded
+	while (lo >= 0) {
+		if (this->str[lo] != e.str[lo]) return true;
+		lo--;
+	}
+
+	return false;
+}
+
+estring estring::erase(int x, int y)
+{
+	int l = this->length();
+	if (x < 0 || x > y || y > l) return "Error";
+	char* temp = this->str;
+	this->str = new char[l - (y - x)];
+	for (int i = 0, j = 0; i <= l; i++, j++)
+		if (j < x || j > y)
+			str[i] = temp[j];
+		else
+			i--;
+	delete[] temp;
+	return this->str;
+}
+
+
 estring::~estring() {
   // free old memory
   delete [] str;
